ea9944372ef80c088f9df7fdd42849c74e09a668
Elijah wines <elijahwines84@yahoo.com>
2020-03-24 16:11:14 +0000

    first commit

---
+++ config/environment.rb
@@ -0,0 +1,7 @@
+require 'httparty'
+require 'pry'
+
+#require_relative "files in the app"
+require_relative "../lib/cli_meal_db/cli_meal_db/version"
+require_relative "../lib/cli_meal_db/api"
+require_relative "../lib/cli_meal_db/cli"
---
+++ lib/cli_meal_db.rb
@@ -0,0 +1,6 @@
+require "cli_meal_db/version"
+
+module CliMealDb
+  class Error < StandardError; end
+  # Your code goes here...
+end
