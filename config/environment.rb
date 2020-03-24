d2b3562c17c13d97c6aee2ac87af11cca212af76
Elijah wines <elijahwines84@yahoo.com>
2020-03-24 17:17:58 +0000

    fixed first require relative

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,6 @@ require 'httparty'
 require 'pry'
 
 #require_relative "files in the app"
-require_relative "../lib/cli_meal_db/cli_meal_db/version"
+require_relative "../lib/cli_meal_db/version"
 require_relative "../lib/cli_meal_db/api"
 require_relative "../lib/cli_meal_db/cli"
