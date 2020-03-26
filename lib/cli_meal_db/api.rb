a03e50be991fffc3f363dbfb3b57484a174cdc21
Elijah wines <elijahwines84@yahoo.com>
2020-03-26 16:59:26 +0000

    category list can display

--- lib/cli_meal_db/api.rb
+++ lib/cli_meal_db/api.rb
@@ -1,14 +1,14 @@
 class API
-  def self.get_main_ingredient
-    response = HTTParty.get("https://www.themealdb.com/api/json/v1/1/list.php?i=list")
+  def self.get_meal_categories
+    response = HTTParty.get("https://www.themealdb.com/api/json/v1/1/list.php?c=list")
 
     #binding.pry
 
-    @@all_main_ingredients = response[:meals].collect {|meals_hash| meals_hash['strIngredient']}
+    @@all_meal_categories = response['meals'].map {|meals_hash| meals_hash['strCategory']}
   end
 
-  def self.all_main_ingredients
-    @@all_main_ingredients
+  def self.all_meal_categories
+    @@all_meal_categories
   end
-  
+
 end
