d54282298887cefa3d523232abaa6313dc084e5a
Elijah wines <elijahwines84@yahoo.com>
2020-03-26 17:05:41 +0000

    cleaned up some code

--- lib/cli_meal_db/api.rb
+++ lib/cli_meal_db/api.rb
@@ -1,13 +1,10 @@
 class API
-  def self.get_meal_categories
+  def self.get_meal_categories # grabs info from the API
     response = HTTParty.get("https://www.themealdb.com/api/json/v1/1/list.php?c=list")
-
-    #binding.pry
-
     @@all_meal_categories = response['meals'].map {|meals_hash| meals_hash['strCategory']}
   end
 
-  def self.all_meal_categories
+  def self.all_meal_categories #displays the list of meal categories
     @@all_meal_categories
   end
 
