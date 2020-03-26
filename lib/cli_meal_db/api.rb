e910e924c0f5360e1906d124299c1f05ea323a2d
Elijah wines <elijahwines84@yahoo.com>
2020-03-26 16:16:55 +0000

    edited api.rb

--- lib/cli_meal_db/api.rb
+++ lib/cli_meal_db/api.rb
@@ -2,8 +2,13 @@ class API
   def self.get_main_ingredient
     response = HTTParty.get("https://www.themealdb.com/api/json/v1/1/list.php?i=list")
 
-    binding.pry
+    #binding.pry
 
-    response[:meals]
+    @@all_main_ingredients = response[:meals].collect {|meals_hash| meals_hash['strIngredient']}
   end
+
+  def self.all_main_ingredients
+    @@all_main_ingredients
+  end
+  
 end
