63112b62117a6e2702e4435d5f36df0aa60408e5
Elijah wines <elijahwines84@yahoo.com>
2020-03-27 18:28:45 +0000

    changed api to different API

--- lib/cli_meal_db/api.rb
+++ lib/cli_meal_db/api.rb
@@ -1,11 +1,28 @@
 class API
-  def self.get_meal_categories # grabs info from the API
-    response = HTTParty.get("https://www.themealdb.com/api/json/v1/1/list.php?c=list")
-    @@all_meal_categories = response['meals'].map {|meals_hash| meals_hash['strCategory']}
+
+  def self.get_random_meal
+    response = HTTParty.get("https://themealdb.com/api/json/v1/1/random.php")
+    @@random_meal = response['meals'].each do |key, value|
+      key.each do |key, value|
+        puts "#{key}: #{value}"
+        puts ""
+        sleep (1)
+      end
+    end
   end
 
-  def self.all_meal_categories #displays the list of meal categories
-    @@all_meal_categories
+  def self.random_meal
+    @@random_meal
   end
 
 end
+
+#  def self.get_random_meal # grabs random meal from the API
+#    random_meal_response = HTTParty.get("https://www.themealdb.com/api/json/v1/1/list.php?c=list")
+
+#    @name = random_meal_response['meals'].map {|meal_hash| meal_hash['strMeal']}
+#    @category = random_meal_response['meals'].map {|meal_hash| meal_hash['strCategory']}
+#    @instructions = random_meal_response['meals'].map {|meal_hash| meal_hash['strInstructions']}
+#    @ingredients = random_meal_response['meals'].map {|meal_hash| meal_hash['strIngredient']}
+#    Meal.new(@name, @category, @instructions, @ingredients)
+#  end
