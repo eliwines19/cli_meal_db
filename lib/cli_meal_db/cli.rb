54d5ef767bcde037252f5850df25ac72af60fbbd
Elijah wines <elijahwines84@yahoo.com>
2020-03-27 14:20:56 +0000

    began user input

--- lib/cli_meal_db/cli.rb
+++ lib/cli_meal_db/cli.rb
@@ -10,7 +10,7 @@ class CLI
     sleep(3)
     puts "Ready to find a meal that's right for you?"
     sleep(3)
-    puts "Please select a number listed below to find the right meal for you!"
+    puts "Please type in the number of the category you're interested in and press enter to learn more!"
     sleep(3)
   end
 
@@ -20,10 +20,19 @@ class CLI
     self.display_meal_categories
   end
 
-  def display_meal_categories # takes meal categories from API and puts into numbered list 
+  def display_meal_categories # takes meal categories from API and puts into numbered list
     API.all_meal_categories.each.with_index(1) do |ingredient, index|
       puts index.to_s + '.) ' + ingredient
     end
+    get_input
+  end
+
+  def get_input # gets the input from the user
+    @input = gets.strip
+  end
+
+  def user_input
+    
   end
 
 end
