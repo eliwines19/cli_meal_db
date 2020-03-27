529cfa61a57dd01a1bd45ede1f000ea97edeaad2
Elijah wines <elijahwines84@yahoo.com>
2020-03-27 18:29:21 +0000

    changed cli

--- lib/cli_meal_db/cli.rb
+++ lib/cli_meal_db/cli.rb
@@ -1,38 +1,48 @@
 class CLI
 
-  def begin_app #calls the welcome method, then the get_meal_categories method
-    self.welcome
-    self.get_meal_categories
-  end
-
-  def welcome # welcomes the user
-    puts "Welcome to mymealfinder!"
-    sleep(3)
-    puts "Ready to find a meal that's right for you?"
-    sleep(3)
-    puts "Please type in the number of the category you're interested in and press enter to learn more!"
-    sleep(3)
-  end
-
-  def get_meal_categories # grabs info from API, calls tge display_meal_categories method
-    API.get_meal_categories
-    API.all_meal_categories
-    self.display_meal_categories
-  end
-
-  def display_meal_categories # takes meal categories from API and puts into numbered list
-    API.all_meal_categories.each.with_index(1) do |ingredient, index|
-      puts index.to_s + '.) ' + ingredient
+  def call
+    #API.get_random_meal
+    puts "Welcome to MealOfTheDay!"
+    puts ""
+    sleep(2)
+    @input = nil
+    menu
+    while @input != "exit"
+      @input = gets.strip
+      if @input == "show meal"
+        puts "This is the meal of the day!"
+        sleep(2)
+        API.get_random_meal
+      elsif @input == "menu"
+        menu
+      elsif @input == "exit"
+        puts "Thanks for checking out the MealOfTheDay app! See you again soon!"
+        break
+      else
+        puts "Sorry! that didn't register with me.. try a different input "
+      end
     end
-    get_input
   end
 
-  def get_input # gets the input from the user
-    @input = gets.strip
+  def menu
+    puts "For information on todays MealOfTheDay, type 'show meal' and press enter:"
+    sleep(2)
+    puts "type 'exit' to leave the app"
+    sleep(2)
+    puts "type 'menu' to return to the menu"
   end
-
-  def user_input
-    
-  end
-
 end
+
+
+    #def get_random_meal # grabs info from API, calls the display_meal_categories method
+    #  API.get_meal_categories
+    #  API.all_meal_categories
+    #  self.display_meal_categories
+    #end
+
+    #def display_meal_categories # takes meal categories from API and puts into numbered list
+    #  API.all_meal_categories.each.with_index(1) do |ingredient, index|
+    #    puts index.to_s + '.) ' + ingredient
+    #  end
+    #  API.get_meals
+    #end
