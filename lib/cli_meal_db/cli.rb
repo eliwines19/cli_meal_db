815a18e74bb77f0ec9636d38526bb16d9f31033e
Elijah wines <elijahwines84@yahoo.com>
2020-03-30 20:47:30 +0000

    changed CLI

--- lib/cli_meal_db/cli.rb
+++ lib/cli_meal_db/cli.rb
@@ -1,35 +1,65 @@
 class CLI
 
   def call
-    puts "Welcome to MealOfTheDay!"
+    welcome
+    main_menu
+  end
+
+  def welcome
+    puts "Welcome to the SmarterEatingHabits app!"
     puts ""
-    sleep(2)
-    @input = nil
-    menu
-    while @input != "exit"
-      @input = gets.strip
-      if @input == "meal"
-        puts "This is the meal of the day!"
-        sleep(2)
-        API.get_random_meal
-      elsif @input == "menu"
-        menu
-      elsif @input == "exit"
-        puts "Thanks for checking out the MealOfTheDay app! See you again soon!"
-        sleep(1)
-        break
-      else
-        puts "Sorry! that didn't register with me.. try a different input "
-      end
+  end
+
+  def list_vegetarian_meals
+    puts "Vegetarian Meals:"
+    @veg_meals = Meal.veg_meals
+    @veg_meals.each.with_index(1) do |meal|
+      puts "#{meal.name}"
+      puts ""
+      sleep(1)
     end
   end
 
-  def menu
-    puts "For information on todays MealOfTheDay, type 'meal' and press enter:"
+  def list_vegan_meals
+    puts "Vegan meals:"
     sleep(2)
-    puts "type 'exit' to leave the app"
-    sleep(2)
-    puts "type 'menu' to return to the menu"
+    @vegan_meals = Meal.vegan_meals
+    @vegan_meals.each.with_index(1) do |meal|
+      puts "#{meal.name}"
+      puts ""
+      sleep(1)
+    end
+  end
+
+  def goodbye
+    puts "Thanks for checking out the SmarterEatingHabits app! See you again soon!"
+    sleep(1)
+  end
+
+  def main_menu
+    input = nil
+    while input != "exit"
+      sleep(0.5)
+      puts "------------------------------"
+      puts "Enter '1' for Vegetarian meals"
+      sleep(0.5)
+      puts "Enter '2' for Vegan meals"
+      sleep(0.5)
+      puts "Enter 'exit' to exit the app"
+      puts "------------------------------"
+      input = gets.strip.downcase
+
+      if input == "1"
+        list_vegetarian_meals
+      elsif input == "2"
+        list_vegan_meals
+      elsif input == "exit"
+        goodbye
+        break
+      else
+        "Didn't catch that... try a different input!"
+      end
+    end
   end
 
 end
