96cc6dc0e3a3ce398c106613f27587c3f4c46457
Elijah wines <elijahwines84@yahoo.com>
2020-03-30 20:48:58 +0000

    put api and initialize in meal.rb

--- lib/cli_meal_db/meal.rb
+++ lib/cli_meal_db/meal.rb
@@ -1,16 +1,40 @@
 class Meal
-  attr_accessor :name, :category, :instructions
-  @@all = []
+  attr_accessor :name
 
-  def initialize(name, category, instructions)
-    @name = name
-    @category = category
-    @instructions = instructions
-    @@all << self 
+  def self.veg_meals
+    self.pull_veg_meals
   end
 
-  def self.all
-    @@all
+  def self.pull_veg_meals
+    veg_meals = []
+    veg_meals << self.list_vegetarian_meals
+  end
+
+  def self.list_vegetarian_meals
+    api = HTTParty.get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegetarian")
+
+    meal = self.new
+    meal.name = api['meals'].map {|hash| hash['strMeal']}
+
+    meal
+  end
+
+  def self.list_vegan_meals
+    api = HTTParty.get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegan")
+
+    meal = self.new
+    meal.name = api['meals'].map {|hash| hash['strMeal']}
+
+    meal
+  end
+
+  def self.vegan_meals
+    self.pull_vegan_meals
+  end
+
+  def self.pull_vegan_meals
+    vegan_meals = []
+    vegan_meals << self.list_vegan_meals
   end
 
 end
