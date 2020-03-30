54e2dcec65bdbaf380a445b4edcdda7c0e081a28
Elijah wines <elijahwines84@yahoo.com>
2020-03-30 14:56:07 +0000

    minor tweaks

--- lib/cli_meal_db/cli.rb
+++ lib/cli_meal_db/cli.rb
@@ -1,7 +1,6 @@
 class CLI
 
   def call
-    #API.get_random_meal
     puts "Welcome to MealOfTheDay!"
     puts ""
     sleep(2)
@@ -9,7 +8,7 @@ class CLI
     menu
     while @input != "exit"
       @input = gets.strip
-      if @input == "show meal"
+      if @input == "meal"
         puts "This is the meal of the day!"
         sleep(2)
         API.get_random_meal
@@ -17,6 +16,7 @@ class CLI
         menu
       elsif @input == "exit"
         puts "Thanks for checking out the MealOfTheDay app! See you again soon!"
+        sleep(1)
         break
       else
         puts "Sorry! that didn't register with me.. try a different input "
@@ -25,24 +25,11 @@ class CLI
   end
 
   def menu
-    puts "For information on todays MealOfTheDay, type 'show meal' and press enter:"
+    puts "For information on todays MealOfTheDay, type 'meal' and press enter:"
     sleep(2)
     puts "type 'exit' to leave the app"
     sleep(2)
     puts "type 'menu' to return to the menu"
   end
+
 end
-
-
-    #def get_random_meal # grabs info from API, calls the display_meal_categories method
-    #  API.get_meal_categories
-    #  API.all_meal_categories
-    #  self.display_meal_categories
-    #end
-
-    #def display_meal_categories # takes meal categories from API and puts into numbered list
-    #  API.all_meal_categories.each.with_index(1) do |ingredient, index|
-    #    puts index.to_s + '.) ' + ingredient
-    #  end
-    #  API.get_meals
-    #end
