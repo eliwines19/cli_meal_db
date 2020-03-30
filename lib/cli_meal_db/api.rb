d39bbc44f896549a2c5f3ff347d56688b18a5c5c
Elijah wines <elijahwines84@yahoo.com>
2020-03-30 14:28:18 +0000

    deleted comments

--- lib/cli_meal_db/api.rb
+++ lib/cli_meal_db/api.rb
@@ -16,13 +16,3 @@ class API
   end
 
 end
-
-#  def self.get_random_meal # grabs random meal from the API
-#    random_meal_response = HTTParty.get("https://www.themealdb.com/api/json/v1/1/list.php?c=list")
-
-#    @name = random_meal_response['meals'].map {|meal_hash| meal_hash['strMeal']}
-#    @category = random_meal_response['meals'].map {|meal_hash| meal_hash['strCategory']}
-#    @instructions = random_meal_response['meals'].map {|meal_hash| meal_hash['strInstructions']}
-#    @ingredients = random_meal_response['meals'].map {|meal_hash| meal_hash['strIngredient']}
-#    Meal.new(@name, @category, @instructions, @ingredients)
-#  end
