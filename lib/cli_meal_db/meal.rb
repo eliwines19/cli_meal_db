1ff7d0bfbaec49727caef567d9d47e97393d84d8
Elijah wines <elijahwines84@yahoo.com>
2020-03-30 14:56:44 +0000

    initialized new meal instances

--- lib/cli_meal_db/meal.rb
+++ lib/cli_meal_db/meal.rb
@@ -1,16 +1,16 @@
 class Meal
-  attr_accessor :name, :category, :instructions, :ingredients
+  attr_accessor :name, :category, :instructions
   @@all = []
 
-  def initialize(name, category, instructions, ingredients)
+  def initialize(name, category, instructions)
     @name = name
     @category = category
     @instructions = instructions
-    @ingredients = ingredients
-    @@all << self
+    @@all << self 
   end
 
   def self.all
     @@all
   end
+
 end
