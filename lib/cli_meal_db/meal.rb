997fbc5af1c9936e4effa7c4fde3270644afcd37
Elijah wines <elijahwines84@yahoo.com>
2020-03-31 18:26:09 +0000

    meal.name and meal.image_url defined

--- lib/cli_meal_db/meal.rb
+++ lib/cli_meal_db/meal.rb
@@ -1,8 +1,13 @@
 class Meal
   attr_accessor :name, :image_url
 
-  def self.veg_meals
-    self.pull_veg_meals
+  def self.list_vegetarian_meals
+    api = HTTParty.get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegetarian")
+
+    meal = self.new
+    meal.name = api['meals'].each {|hash| hash['strMeal']}
+    meal.image_url = api['meals'].each {|hash| hash['strMealThumb']}
+    meal
   end
 
   def self.pull_veg_meals
@@ -10,32 +15,27 @@ class Meal
     veg_meals << self.list_vegetarian_meals
   end
 
-  def self.list_vegetarian_meals
-    api = HTTParty.get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegetarian")
-
-    meal = self.new
-    meal.name = api['meals'].map {|hash| hash['strMeal']}
-
-    meal
+  def self.veg_meals
+    self.pull_veg_meals
   end
+  
 
   def self.list_vegan_meals
     api = HTTParty.get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegan")
 
     meal = self.new
-    meal.name = api['meals'].map {|hash| hash['strMeal']}
-    meal.image_url = api['meals'].map {|hash| hash['strMealThumb']}
-
+    meal.name = api['meals'].each {|hash| hash['strMeal']}
+    meal.image_url = api['meals'].each {|hash| hash['strMealThumb']}
     meal
-  end
-
-  def self.vegan_meals
-    self.pull_vegan_meal
   end
 
   def self.pull_vegan_meal
     vegan_meals = []
     vegan_meals << self.list_vegan_meals
+  end
+
+  def self.vegan_meals
+    self.pull_vegan_meal
   end
 
 end
