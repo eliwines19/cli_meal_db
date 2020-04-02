acb629c63df49c8fc522d079ea007c807898275f
Elijah wines <elijahwines84@yahoo.com>
2020-04-02 17:48:41 +0000

    cleaned up unnecessary code

--- lib/cli_meal_db/meal.rb
+++ lib/cli_meal_db/meal.rb
@@ -1,7 +1,7 @@
 class Meal
   attr_accessor :name, :image_url
 
-  def self.list_vegetarian_meals
+  def self.get_vegetarian_meals
     api = HTTParty.get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegetarian")
 
     meal = self.new
@@ -10,17 +10,13 @@ class Meal
     meal
   end
 
-  def self.pull_veg_meals
+  def self.veg_meals_array 
     veg_meals = []
-    veg_meals << self.list_vegetarian_meals
+    veg_meals << self.get_vegetarian_meals
+    veg_meals
   end
 
-  def self.veg_meals
-    self.pull_veg_meals
-  end
-  
-
-  def self.list_vegan_meals
+  def self.get_vegan_meals
     api = HTTParty.get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegan")
 
     meal = self.new
@@ -29,13 +25,10 @@ class Meal
     meal
   end
 
-  def self.pull_vegan_meal
+  def self.vegan_meals_array
     vegan_meals = []
-    vegan_meals << self.list_vegan_meals
-  end
-
-  def self.vegan_meals
-    self.pull_vegan_meal
+    vegan_meals << self.get_vegan_meals
+    vegan_meals
   end
 
 end
