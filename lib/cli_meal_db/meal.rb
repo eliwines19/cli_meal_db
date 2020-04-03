8469a105d78cca9f6995b0747623403f6040fbfb
Elijah wines <elijahwines84@yahoo.com>
2020-04-03 17:50:06 +0000

    meal.rb can save instances of Meals

--- lib/cli_meal_db/meal.rb
+++ lib/cli_meal_db/meal.rb
@@ -11,23 +11,4 @@ class Meal
     @@all
   end
 
-  def self.veg_meals_array
-    veg_meals = []
-    veg_meals << self.get_vegetarian_meals
-    veg_meals
-  end
-
-  def self.vegan_meals_array
-    vegan_meals = []
-    vegan_meals << self.get_vegan_meals
-    vegan_meals
-  end
-
-  def self.all_meals_array
-    all_meals = []
-    all_meals << self.get_vegetarian_meals
-    all_meals << self.get_vegan_meals
-    all_meals
-  end
-
 end
