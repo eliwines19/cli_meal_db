8c1407b1eddfcace5de0ccbc8d41f660b0993902
Elijah wines <elijahwines84@yahoo.com>
2020-04-03 17:48:59 +0000

    api now accesses random meal api

--- lib/cli_meal_db/api.rb
+++ lib/cli_meal_db/api.rb
@@ -1,28 +1,19 @@
 class API
-  def self.get_vegan_meals
-    api_response = HTTParty.get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegan")
 
-    api_response['meals'].each do |hash| hash['strMeal']
+  def self.get_random_meal
+    api_response = HTTParty.get("https://www.themealdb.com/api/json/v1/1/random.php")
+
+    api_response['meals'].each do |hash|
       meal = Meal.new
-      binding.pry
-      meal.name =
-      meal.image_url =
-      meal.api_id =
+      meal.name = hash['strMeal']
+      meal.image_url = hash['strMealThumb']
+      meal.api_id = hash['idMeal']
+      meal.meal_category = hash['strCategory']
+      meal.meal_instructions = hash['strInstructions']
+      meal.meal_area = hash['strArea']
+      meal.save
+      meal
     end
-    meal.name = meal.meals.map {|key| key['strMeal']}
-    meal.image_url = meal.meals.map {|key| key['strMealThumb']}
-    meal
   end
 
-
-
-  def self.get_vegetarian_meals
-    api = HTTParty.get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegetarian")
-
-    meal = self.new
-    meal.meals = api['meals'].each {|hash| hash['strMeal']}
-    meal.name = meal.meals.map {|key| key['strMeal']}
-    meal.image_url = meal.meals.map {|key| key['strMealThumb']}
-    meal
-  end
 end
