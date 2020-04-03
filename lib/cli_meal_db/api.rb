b2a8792b79b411cf576b4b5c5c8d71934486cad9
Elijah wines <elijahwines84@yahoo.com>
2020-04-03 16:36:04 +0000

    calling api

--- lib/cli_meal_db/api.rb
+++ lib/cli_meal_db/api.rb
@@ -2,12 +2,19 @@ class API
   def self.get_vegan_meals
     api_response = HTTParty.get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegan")
 
-    meal = self.new
-    meal.meals = api['meals'].each {|hash| hash['strMeal']}
+    api_response['meals'].each do |hash| hash['strMeal']
+      meal = Meal.new
+      binding.pry
+      meal.name =
+      meal.image_url =
+      meal.api_id =
+    end
     meal.name = meal.meals.map {|key| key['strMeal']}
     meal.image_url = meal.meals.map {|key| key['strMealThumb']}
     meal
   end
+
+
 
   def self.get_vegetarian_meals
     api = HTTParty.get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegetarian")
